6b6af0c53a3a74c7287dbb1d41d532a7c67ef9a1
José Oliveros <jose.oliveros.1983@gmail.com>
2018-03-10 13:13:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 install_requires = [
     'bintrees==2.0.7',
     'requests==2.13.0',
-    'six==1.10.0',
-    'websocket-client==0.40.0',
+    'six',
+    'websocket-client',
     'pymongo==3.5.1'
 ]
 
